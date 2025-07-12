565103fbadf98ce9b1e989cf9809157df2995c81
Mayank Raj <88675779+mayankraj25@users.noreply.github.com>
2025-07-12 14:23:31 +0530

    Update qa_chain.py

--- chains/qa_chain.py
+++ chains/qa_chain.py
@@ -1,9 +1,9 @@
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 
-def build_qa_chain(vectorstore):
+def build_qa_chain(vectorstore,api_key: str):
     retriever=vectorstore.as_retriever()
-    llm=ChatOpenAI(model="gpt-4o-mini",temperature=0.1)
+    llm=ChatOpenAI(model="gpt-4o-mini",temperature=0.1,openai_api_key=api_key)
     qa_chain=RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
