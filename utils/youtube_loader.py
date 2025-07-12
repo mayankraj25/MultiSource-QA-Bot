23bda771ac5ebbdbf98b1cb9d41e63ae0cf0c72a
Mayank Raj <88675779+mayankraj25@users.noreply.github.com>
2025-07-12 14:45:55 +0530

    Update youtube_loader.py

--- utils/youtube_loader.py
+++ utils/youtube_loader.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import YoutubeLoader
+from youtube_transcript_api import YouTubeTranscriptApi
 
 def load_youtube(video_url):
     loader = YoutubeLoader.from_youtube_url(video_url)
